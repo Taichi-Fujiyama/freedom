e8cea93f266c35ce17156fdec4584543685d5358
Taichi Fujiyama <taichi0810withk@gmail.com>
2021-06-08 09:45:21 +0000

    ビュー（ホーム画面）の微調整後のコミットです

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,5 @@ require "csv"
 
 CSV.foreach("db/csv_for_seed.csv") do |info|
   Code.create(:cd => info[0],:cd_type =>info[1],:name =>info[2])
+  # infoのインデックス番号はmysqlのテーブルではなくて、実際のcsvファイルのカラム順にする
 end
