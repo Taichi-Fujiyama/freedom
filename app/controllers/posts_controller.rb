e315fb7e02c0164f8a0fd64f141d15edafd7fa79
Taichi Fujiyama <taichi0810withk@gmail.com>
2021-06-11 00:03:35 +0000

    円グラフの凡例追加と、業務一覧の枠追加に関するコミットです

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,15 @@ class PostsController < ApplicationController
   end
   
   def index
+    #間に合わせの各業務ステータスの値
+    @complete = 40
+    @in_progress = 10
+    @not_yet = 30
+    # 以下は、円グラフの凡例を導入するためのコード
+    sum = @complete + @in_progress + @not_yet
+    @complete_per = sprintf("%.1f",@complete/sum.to_f * 100)
+    @in_progress_per = sprintf("%.1f",@in_progress/sum.to_f * 100)
+    @not_yet_per = sprintf("%.1f",@not_yet/sum.to_f * 100)
+    
   end
 end
