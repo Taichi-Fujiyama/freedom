29efa1280da175c30cf817a3b648557f70038ff8
Taichi Fujiyama <taichi0810withk@gmail.com>
2021-06-13 07:24:25 +0000

    業務内容表示までのコミットです

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-  def create
-    @post = Post.new(
-      user_id:@current_user.id
-      )
-  end
+  # def create
+  #   @post = Post.new(
+  #     user_id:@current_user.id
+  #     )
+  # end
   
   def index
     #間に合わせの各業務ステータスの値
@@ -15,6 +15,30 @@ class PostsController < ApplicationController
     @complete_per = sprintf("%.1f",@complete/sum.to_f * 100)
     @in_progress_per = sprintf("%.1f",@in_progress/sum.to_f * 100)
     @not_yet_per = sprintf("%.1f",@not_yet/sum.to_f * 100)
+    #追加した業務を一覧で表示するためのコード
+    @posts = Post.all
     
   end
+  
+  #投稿の追加に関するコントローラーを設定する
+  def new
+    @post = Post.new
+  end
+  
+  #カリキュラムを参照にしたが、以下の@postに代入したコードの内容、newとcreateの関係がイマイチよくわからない。
+  def create
+    @post = current_user.posts.new(topic_params)
+     if @post.save 
+       redirect_to home_path, success:"業務を追加しました。"
+     else
+       redirect_to new_post_path,danger:"業務を追加できませんでした。入力項目に不備があります。"
+     end
+  end
+  
+  private 
+  def post_params
+    params.require(:topic).permit(:title,:description,:work_hour,:due_date,:URL,:appointed_user_id)
+  
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
   
-
+  #投稿（業務提出)に関するルーティングを設定する
+  resources :posts
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
