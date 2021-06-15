cc96f67015c6eabfbaafe7d476ca07c053639f78
Taichi Fujiyama <taichi0810withk@gmail.com>
2021-06-15 07:33:25 +0000

    業務追加画面のビュー完成までのコミットです

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
     @complete = Post.where(status:"1").count
     @in_progress = Post.where(status:"2").count
     @not_yet = Post.where(status:"3").count
+    
     # 以下は、円グラフの凡例を導入するためのコード
     sum = @complete + @in_progress + @not_yet
     @complete_per = sprintf("%.1f",@complete/sum.to_f * 100)
@@ -27,20 +28,30 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   
-  #カリキュラムを参照にしたが、以下の@postに代入したコードの内容、newとcreateの関係がイマイチよくわからない。
+  #投稿の追加ボタンが押された時のデータ受け渡しの処理
   def create
-    @post = current_user.posts.new(topic_params)
+    @post = current_user.posts.new(post_params)
+    # 上記の書き方は、省略した書き方となる。基本に従って書くと以下の様になる
+    # @post = Post.new
+    # @post.user_id = current_user.id
+    # @post.title = post_params[:title]
+    # @post.description = post_params[:description]
+    # @post.work_hour = post_params[:work_hour]
+    # @post.due_date = post_params[:due_date]
+    # @post.URL = post_params[:URL]
+    # @post.appointed_user_id = post_params[:appointed_user_id]
+    
      if @post.save 
        redirect_to home_path, success:"業務を追加しました。"
      else
-       redirect_to new_post_path,danger:"業務を追加できませんでした。入力項目に不備があります。"
+       flash.now[:danger] = "業務を追加できませんでした。タイトルと業務内容、優先度は必ず記入してください。"
+       render :new 
      end
   end
   
   private 
   def post_params
-    params.require(:topic).permit(:title,:description,:work_hour,:due_date,:URL,:appointed_user_id)
-  
+    params.require(:post).permit(:title,:description,:work_hour,:due_date,:priority,:URL,:appointed_user_id)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   # ホーム画面で業務を一覧表示させるため、indexアクションとする
   get  "/home" => "posts#index" 
   
-  resources :users
   
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
   
-  #投稿（業務提出)に関するルーティングを設定する
-  resources :posts
   
+  resources :users
+  resources :posts
+  #投稿（業務提出)に関するルーティングを設定する
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
