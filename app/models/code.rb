4e3c37981c86d242293326126665a98992aabc66
Taichi Fujiyama <taichi0810withk@gmail.com>
2021-06-24 07:56:23 +0000

    機能完成までのコミットです

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,6 +1,5 @@
 class Code < ApplicationRecord
   # scope:任意の名前,->(引数（任意の名前)　{ where(カラム名: "カラム内の値).where(cd: value) }
-  # has_many :posts
   scope :status, ->(value) { where(cd_type: "status").where(cd: value).first }
   
   #updeteで進捗の変更に使うためのもの
